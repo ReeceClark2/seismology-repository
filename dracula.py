d6747736491cff42742400b5b43935c8654bf3b9
ReeceClark2 <reececlark55@gmail.com>
2026-09-18 17:29:01 -0400

    small fix

--- dracula.py
+++ dracula.py
@@ -108,7 +108,7 @@ def run_initial_conditions_worker(t, d, signal_space, depth, grid_search_args, n
 
     signals.append(signal_candidate)
     model = bats.get_model(subband_t, subband_d, signals)
-    utils.plot_time_series(path / f"{len(signals_with_candidate)}_signal_time_series.png", subband_t, subband_d, f"Time Series for {len(signals_with_candidate)} Signal Model", model)
+    utils.plot_time_series(path / f"{len(signals)}_signal_time_series.png", subband_t, subband_d, f"Time Series for {len(signals)} Signal Model", model)
 
     glob_ll_0 = bats.get_glob_ll(t[mask], d[mask], signals)
 
